cba316d4abd9e3cf91aa75e5111d18b13ff01d96
Nirali Pachchigar <nnpachchigar1@buffs.wtamu.edu>
2023-03-09 07:46:42 -0600

    change commit

--- Barky/tests/test_api.py
+++ Barky/tests/test_api.py
@@ -30,19 +30,16 @@ def test_one(api):
     assert response.data == f'The provided id is 123'
 
 def test_all(api):
-    with app.test_client() as client:
-        response = client.get('/api/all')
-        assert response.status_code == 200
-        assert response.data == b'all records'
+    response = requests.get('/api/all')
+    assert response.status_code == 200
+    assert response.data == b'all records'
 
 def test_first(api):
-    with app.test_client() as client:
-        response = client.get('/api/first/property/value/sort')
-        assert response.status_code == 200
-        assert response.data == b'the first '
+    response = requests.get('/api/first/property/value/sort')
+    assert response.status_code == 200
+    assert response.data == b'the first '
 
 def test_many(api):
-    with app.test_client() as client:
-        response = client.get('/api/many/property/value/sort')
-        assert response.status_code == 200
-        assert response.data == b'many records'
+    response = requests.get('/api/many/property/value/sort')
+    assert response.status_code == 200
+    assert response.data == b'many records'
